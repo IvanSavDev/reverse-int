188ad60509a5d1bda1da90b12579bb9da07d7765
Ivan <mortlilport@mail.ru>
2022-01-13 14:10:10 +0300

    submit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  const moduloNumber = Math.abs(n);
+  const numberToString = String(moduloNumber);
+  let newNumber = "";
+
+  for (let i = numberToString.length - 1; i >= 0; i--) {
+    newNumber += numberToString[i];
+  }
+
+  return newNumber;
 }
